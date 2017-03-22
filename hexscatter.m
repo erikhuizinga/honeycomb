89d9a47b468c003ee4861fe099e8c77c9dd57bbe
Erik Huizinga <erik.huizinga@nedap.com>
2017-03-22 16:56:21 +0100

    Restyle, improve and simplify code partially

--- hexscatter.m
+++ hexscatter.m
@@ -1,4 +1,4 @@
-function h = hexscatter(xdata, ydata, varargin)
+function h = hexscatter(xData, yData, varargin)
 % HEXSCATTER Create a scatter plot using hexagonal binning
 %
 %   HEXSCATTER(x, y) creates a scatter plot using hexagonal binning of the
@@ -22,21 +22,21 @@ function h = hexscatter(xdata, ydata, varargin)
 %   corresponding value. Name must appear inside single quotes (' '). You
 %   can specify several name and value pair arguments in any order as
 %   Name1, Value1, ..., NameN, ValueN.
-%   Example: 'xlim', [0, 1], 'ylim', [1, 10]
+%   Example: 'XLim', [0, 1], 'YLim', [1, 10]
 %
-%   'xlim': x-axis limits
+%   'XLim': x-axis limits
 %   [min(x(:)), max(x(:))] (default) | two-element numeric vector
 %   The x-axis limits specified as a two-element numeric vector. The first
 %   and second element correspond to the left and right x-axis limit
 %   respectively. The second element must be greater than the first.
 %
-%   'ylim': y-axis limits
+%   'YLim': y-axis limits
 %   [min(y(:)), max(y(:))] (default) | two-element numeric vector
 %   The y-axis limits specified as a two-element numeric vector. The first
 %   and second element correspond to the lower and upper y-axis limit
 %   respectively. The second element must be greater than the first.
 %
-%   'res': resolution
+%   'Resolution': resolution of hexagons
 %   [100, 100] (default) | numeric scalar | two-element numeric vector
 %   The hexagonal grid resolution specified either as a scalar, or a
 %   two-element vector. The resolution is the number of hexagons in
@@ -45,11 +45,11 @@ function h = hexscatter(xdata, ydata, varargin)
 %   resolution and the second to the horizontal resolution. If the
 %   specified value is scalar, then its value is used for both directions.
 %
-%   'drawEdges': draw edges around hexagons
+%   'DrawEdges': draw edges around hexagons
 %   false (default) | true
 %   Draw edges around the hexagons if the specified value is true.
 %
-%   'showZeros': show zero count hexagons
+%   'ShowZeros': show zero count hexagons
 %   false (default) | true
 
 % This function is based on hexscatter.m by Gordon Bean, source:
@@ -90,126 +90,170 @@ function h = hexscatter(xdata, ydata, varargin)
 % SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
-%% Convert to vectors
-xdata = xdata(:);
-ydata = ydata(:);
+%% Parse input arguments
+% Convert to vectors
+xData = xData(:);
+yData = yData(:);
 
-nix = isnan(xdata) | isnan(ydata) | isinf(xdata) | isinf(ydata);
-xdata = xdata(~nix);
-ydata = ydata(~nix);
+% Keep only valid indices
+lValid = isnan(xData) | isnan(yData) | isinf(xData) | isinf(yData);
+xData = xData(~lValid);
+yData = yData(~lValid);
 
-params = default_param( varargin, ...
-    'xlim', [min(xdata(:)) max(xdata(:))], ...
-    'ylim', [min(ydata(:)) max(ydata(:))], ...
-    'res', [100 100], ...
-    'drawEdges', false, ...
-    'showZeros', false);
 
-if params.drawedges
-    ec = 'flat';
-else
-    ec = 'none';
+% Parse variable input arguments
+parameters = getDefaultParameters( ...
+    varargin, ...
+    'XLim', [min(xData(:)), max(xData(:))], ...
+    'YLim', [min(yData(:)), max(yData(:))], ...
+    'Resolution', [100, 100], ...
+    'DrawEdges', false, ...
+    'ShowZeros', false ...
+    );
+
+
+% Format resolution parameters
+if length(parameters.Resolution) == 1
+    parameters.Resolution = parameters.Resolution * [1, 1];
 end
+parameters.x.resolution = parameters.Resolution(2);
+parameters.y.resolution = parameters.Resolution(1);
 
-if length(params.res) == 1
-    params.res = params.res * [1, 1];
-end
 
-%% Determine grid
-xl = params.xlim;
-yl = params.ylim;
+% Format limit parameters
+parameters.x.limit = parameters.XLim;
+parameters.y.limit = parameters.YLim;
 
-xbins = linspace(xl(1), xl(2), params.res(2));
-ybins = linspace(yl(1), yl(2), params.res(1));
-dy = diff(ybins([1, 2])) * 0.5;
 
-[X, Y] = meshgrid(xbins, ybins);
-[n, m] = size(X);
-Y(:, 1 : fix(end / 2) * 2) = ...
-    Y(:, 1 : fix(end / 2) * 2) + repmat([0, dy], [n, fix(m / 2)]);
+%% Determine hexagon grid
+% Determine hexagon centre coordinates
+xCenter = linspace( ...
+    parameters.x.limit(1), parameters.x.limit(2), parameters.x.resolution);
+yCenter = linspace( ...
+    parameters.y.limit(1), parameters.y.limit(2), parameters.y.resolution);
+
+% Make X and Y grids of hexagon centres
+[XCenter, YCenter] = meshgrid(xCenter, yCenter);
+
+% Adjust every second hexagon centre in Y
+dy = diff(yCenter([1, 2])) / 2;
+iAdjust = 1 : 2 : numel(xCenter);
+YCenter(:, iAdjust) = YCenter(:, iAdjust) + dy;
+yCenter(iAdjust) = yCenter(iAdjust) + dy; %FIXME
+
 
 %% Map points to boxes
-% Which pair of columns?
-dx = diff(xbins([1, 2]));
-foox = floor((xdata - xbins(1)) ./ dx) + 1;
-foox(foox > length(xbins)) = length(xbins);
-foox(foox < 1) = 1;
+% Determine horizontal and vertical box numbers of data
+dx = diff(xCenter([1, 2]));
+boxX = floor((xData - parameters.x.limit(1)) / dx) + 1;
+boxY = floor((yData - parameters.y.limit(1)) / (2 * dy)) + 1;
 
-% Which pair of rows?
-% Use the first row, which starts without an offset, as the standard
-fooy = floor((ydata - ybins(1)) ./ diff(ybins([1, 2]))) + 1;
-fooy(fooy > length(ybins)) = length(ybins);
-fooy(fooy < 1) = 1;
 
-% Which orientation
-orientation = mod(foox, 2) == 1;
+% Limit box numbers to hexagonal grid range
+%TODO Ignore data points outside grid: user requests grid of a limited
+%     size, i.e., points outside it should not be counted in the grids
+boxX = min(boxX, parameters.x.resolution);
+boxX = max(boxX, 1);
+boxY = min(boxY, parameters.x.resolution);
+boxY = max(boxY, 1);
 
-% Map points to boxes
-xdata = xdata - xbins(foox).';
-ydata = ydata - ybins(fooy).';
+
+% Determine orientation
+orientation = mod(boxX, 2) == 1;
+
+% Shift data to relative position in boxs
+boxXData = xData - xCenter(boxX).';
+boxYData = yData - yCenter(boxY).';
 
 % Which layer
-layer = ydata > dy;
+layer = boxYData > dy;
 
 % Convert to block B format
-toflip = layer == orientation;
-xdata(toflip) = dx - xdata(toflip);
+l2Flip = layer == orientation;
+boxXData(l2Flip) = dx - boxXData(l2Flip);
 
-ydata(layer == 1) = ydata(layer == 1) - dy;
+boxYData(layer == 1) = boxYData(layer == 1) - dy;
+
+%TODO
+newplot
+hold on
+axis equal
+plot(boxXData, boxYData, 'rx')
 
 % Find closest corner
-topright = xdata.^2 + ydata.^2 > (xdata - dx).^2 + (ydata - dy).^2;
-clear xdata ydata
+topright = ...
+    boxXData.^2 + boxYData.^2 > (boxXData - dx).^2 + (boxYData - dy).^2;
+
+%TODO
+table(xData, yData, boxXData, boxYData, boxX, boxY, orientation, layer, topright)
 
 %% Map corners back to bins
-% Which x bin?
-foox = foox + ~(orientation == (layer == topright));
-foox(foox > length(xbins)) = length(xbins);
+% Determine horizontal bin number
+binX = boxX + ~(orientation == (layer == topright));
+binX(binX > length(xCenter)) = length(xCenter);
 
-% Which y bin?
-fooy = fooy + (layer & topright);
-fooy(fooy > length(ybins)) = length(ybins);
+% Determine vertical bin number
+binY = boxY + (layer & topright);
+binY(binY > length(yCenter)) = length(yCenter);
 
-foox = sub2ind(size(X), fooy, foox);
+binX = sub2ind(size(XCenter), binY, binX);
 % Replaced foox to conserve memory
 
+
 %% Determine counts
-counts = histc(foox, 1 : numel(X)).';
+counts = histc(binX, 1 : numel(XCenter));
 
-newplot;
-xscale = diff(xbins([1, 2])) * 2 / 3;
-yscale = diff(ybins([1, 2])) * 2 / 3;
-theta = 0 : 60 : 360;
-x = bsxfun(@plus, X(:), cosd(theta) * xscale).';
-y = bsxfun(@plus, Y(:), sind(theta) * yscale).';
+%newplot; %TODO
+xscale = dx * 2 / 3;
+yscale = dy * 2 / sqrt(3);
+theta = (0 : 1/3 : 2) * pi;
+x = bsxfun(@plus, XCenter(:), cos(theta) * xscale).';
+y = bsxfun(@plus, YCenter(:), sin(theta) * yscale).';
 
-if params.showzeros
-    h = patch(x, y, counts, 'edgeColor', ec);
+%TODO
+plot(XCenter(:), YCenter(:), 'k^')
+plot(x(:), y(:), 'ko')
+
+% Determine patch edgeColor value
+if parameters.DrawEdges
+    patchEdgeColor = 'flat';
 else
-    jj = counts > 0;
-    h = patch(x(:, jj), y(:, jj), counts(jj), 'edgeColor', ec);
+    patchEdgeColor = 'none';
 end
 
-xlim(params.xlim);
-ylim(params.ylim);
+plot(xData, yData, 'ro')
+
+
+% Draw patches
+if parameters.ShowZeros
+    h = patch(x, y, counts, 'edgeColor', patchEdgeColor);
+else
+    jj = counts > 0;
+    h = patch(x(:, jj), y(:, jj), counts(jj), ...
+        'EdgeColor', patchEdgeColor, ...
+        'FaceAlpha', .2);
+end
+
+xlim(parameters.XLim);
+ylim(parameters.YLim);
 
 if nargout == 0
     clear h;
 end
 
-%% Function: default_param
+%% Function: getDefaultParameters
 % Gordon Bean, March 2012
 % Copied from https://github.com/brazilbean/bean-matlab-toolkit
-    function params = default_param(params, varargin)
-        if (iscell(params))
-            params = get_params(params{:});
+    function parameters = getDefaultParameters(parameters, varargin)
+        if (iscell(parameters))
+            parameters = getParameters(parameters{:});
         end
-        defaults = get_params(varargin{:});
+        defaults = getParameters(varargin{:});
         
         for f = fieldnames(defaults).'
             field = f{:};
-            if (~isfield(params, lower(field)))
-                params.(lower(field)) = defaults.(field);
+            if (~isfield(parameters, field))
+                parameters.(field) = defaults.(field);
             end
         end
     end
@@ -224,8 +268,8 @@ end
 % Converts all keys to lower case.
 %
 % Copied from https://github.com/brazilbean/bean-matlab-toolkit
-    function params = get_params(varargin)
-        params = struct;
+    function parameters = getParameters(varargin)
+        parameters = struct;
         
         nn = length(varargin);
         if (mod(nn, 2) ~= 0)
@@ -234,7 +278,7 @@ end
         
         tmp = reshape(varargin, [2, nn / 2]);
         for kk = 1 : size(tmp, 2)
-            params.(lower(tmp{1, kk})) = tmp{2, kk};
+            parameters.(tmp{1, kk}) = tmp{2, kk};
         end
     end
 end
