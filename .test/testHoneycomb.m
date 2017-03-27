97cc48d0486e41ddfcb83bed9048a38075bbce39
Erik Huizinga <erik.huizinga@nedap.com>
2017-03-27 16:29:46 +0200

    Test hexagon grid initialization

--- .test/testHoneycomb.m
+++ .test/testHoneycomb.m
@@ -2,3 +2,24 @@
 
 clear
 close all
+
+
+%% Test discretize
+% Set number of elements
+n = 100;
+
+% Generate x
+x = rand(n, 1);
+x = [0; 0; 1; 1; x];
+x = x(1 : n);
+
+% Generate y
+y = rand(n, 1);
+y = [0; 1; 0; 1; y];
+y = y(1 : n);
+
+% Set number of hexagons in x and y direction
+numBins = 3;
+
+% Test honeycomb
+honeycomb(x, y, numBins)
