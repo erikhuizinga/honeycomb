0f81595bf8561cb608d3de4dc3ffbd5fa797681a
Erik Huizinga <erik.huizinga@nedap.com>
2017-03-28 12:51:34 +0200

    Test more random data

--- .test/testHoneycomb.m
+++ .test/testHoneycomb.m
@@ -6,30 +6,31 @@ close all
 
 %% Test discretize
 % Set number of elements
-n = 30;
+n = 1000;
 
 % Generate x
-x = rand(n, 1);
-x = [0; 0; 1; 1; x];
+x = randn(n, 1);
+x = [-5; -5; 5; 5; x];
 x = x(1 : n);
 
 % Generate y
-y = rand(n, 1);
-y = [0; 1; 0; 1; y];
+y = randn(n, 1);
+y = [-5; 5; -5; 5; y];
 y = y(1 : n);
 
 % Set number of hexagons in x and y direction
-numBins = 3;
+numBins = 10;
 
 % Plot bivariate histogram viewed from the top
 figure
 hold on
 histogram2(x, y, numBins, 'DisplayStyle', 'tile')
-plot(x, y, 'ro', 'MarkerFaceColor', 'r')
+plot(x, y, 'ro')
 
 % Test honeycomb
+figure
 honeycomb(x, y, numBins)
-
+colorbar
 
 %TODO
 % - Test data on edges: 6 edges between hexagons, outer edges of entire grid,
