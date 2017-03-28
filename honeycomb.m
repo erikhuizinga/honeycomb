9f97a8784ac9ba40364904945184e4a7bb5c6a18
Erik Huizinga <erik.huizinga@nedap.com>
2017-03-28 10:08:33 +0200

    Sort hexagons from top to bottom and right to left

--- honeycomb.m
+++ honeycomb.m
@@ -2,6 +2,13 @@ function honeycomb(x, y, numBins)
 % HONEYCOMB
 
 
+%%TODO
+figure
+plot(x(:), y(:), '+k')
+axis tight
+hold on
+
+
 %% Discretize data into bins
 % Get number of bins in both directions
 numXBins = numBins;
@@ -17,6 +24,11 @@ numYBins = numBins;
 % Get range of bins
 xLim = xEdges([1, end]);
 yLim = yEdges([1, end]);
+
+
+%TODO delete me
+plot([xLim(1), xLim, flip(xLim)], [yLim, flip(yLim), yLim(1)], 'k:')
+
 
 % Calculate number of hexagon radii in data limits and calculate distance
 % between hexagon centers
@@ -46,9 +58,13 @@ YCenter(:, isShifted) = YCenter(:, isShifted) + dy/2;
 XCenter = XCenter(:);
 YCenter = YCenter(:);
 
+% Sort center coordinates from top to bottom and right to left
+Centers = flipud(sortrows([YCenter, XCenter]));
+YCenter = Centers(:, 1);
+XCenter = Centers(:, 2);
+
 
 %TODO delete me
-figure
 plot(XCenter, YCenter, 'r^')
 
 
@@ -59,7 +75,6 @@ YVertices = bsxfun(@plus, YCenter, ry * sind(theta));
 
 
 %TODO delete me
-hold on
 plot(XVertices(:), YVertices(:), 'bo')
 
 
